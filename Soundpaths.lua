15e4d796eaad40ddac7eb496b50b98d2c1aa5ca8
xFranklin7 <105977553+xFranklin7@users.noreply.github.com>
2024-05-13 16:23:34 +0200

    Add files via upload

---
+++ Soundpaths.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFFF0000Full Insanity -xFranklin|r", [[Interface\Addons\MyMedia\sound\Full Insanity -xFranklin.ogg]])
+LSM:Register("sound", "|cFFFF00002|r", [[Interface\Addons\SharedMedia_Causese\sound\2.ogg]])
+LSM:Register("sound", "|cFFFF00003|r", [[Interface\Addons\SharedMedia_Causese\sound\3.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
