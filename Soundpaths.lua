6f47b2d58adec75f93638b10aa9767af1564746a
xFranklin7 <105977553+xFranklin7@users.noreply.github.com>
2024-05-13 20:50:21 +0200

    Add files via upload

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -15,9 +15,9 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
-LSM:Register("sound", "|cFF9acd32Full Insanity -xFranklin|r", [[Interface\Addons\MyMedia\sound\Full Insanity -xFranklin.ogg]])
-LSM:Register("sound", "|cFF9acd32Full Stack -xFranklin|r", [[Interface\Addons\MyMedia\sound\Full Stack -xFranklin.ogg]])
-LSM:Register("sound", "|cFF9acd32Shadow Crash UP -xFranklin|r", [[Interface\Addons\MyMedia\sound\Shadow Crash UP -xFranklin.ogg]])
+LSM:Register("sound", "|cFF9acd32Full Insanity -xFranklin|r", [[Interface\Addons\MyMedia\ShadowPriest SoundPack\Full Insanity -xFranklin.ogg]])
+LSM:Register("sound", "|cFF9acd32Full Stack -xFranklin|r", [[Interface\Addons\MyMedia\ShadowPriest SoundPack\Full Stack -xFranklin.ogg]])
+LSM:Register("sound", "|cFF9acd32Shadow Crash UP -xFranklin|r", [[Interface\Addons\MyMedia\ShadowPriest SoundPack\Shadow Crash UP -xFranklin.ogg]])
 
 -- -----
 --   STATUSBAR
